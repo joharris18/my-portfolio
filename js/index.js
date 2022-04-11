4c80c216ed1681a09ca71d574adac46de0e272ba
joharris18 <johnisabelo18@gmail.com>
2022-04-11 15:08:57 +0800

    layout fixes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
+
+
+
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
@@ -10,3 +13,20 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+
+
+var i = 0;
+var txt = 'John Harris />';
+var speed = 150;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector('#full-name').innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter()
+
